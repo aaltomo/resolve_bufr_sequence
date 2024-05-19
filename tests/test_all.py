eedfb5b24f3ac6e8c3511b04dad0447f25db4cb2
Mikko Aalto <aaltomo@gmail.com>
2024-05-19 13:38:18 +0300

    Add a test.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import os
 import pytest
 
 from resolve_bufr_sequence import read_sequence
 
+@pytest.fixture
 def test_read():
     template = read_sequence("307089")
     assert template == "[  307087, 307088 ]"
