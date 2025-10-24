157fe1eeaae336efc9275cc4ffdabb95fa35609d
Mikko Aalto <aaltomo@gmail.com>
2025-10-24 22:36:31 +0300

    More updates

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,6 +2,7 @@
 
 from src.resolve_bufr_sequence.io_utils import reader
 
+
 def test_read():
     template = reader.read_sequence("302046")
-    assert template == { "302046": [ "004024", "004024", "012049" ] }
+    assert template == {"302046": ["004024", "004024", "012049"]}
